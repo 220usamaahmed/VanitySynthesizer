131e7cfcb747e52c5324bbcde0b9d20a63c13dda
Usama Ahmed <220usamaahmed@gmail.com>
2021-09-08 18:26:08 +0500

    Inital commit, basic osc setup

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Synth_00AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    Synth_00AudioProcessorEditor (Synth_00AudioProcessor&);
+    ~Synth_00AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Synth_00AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Synth_00AudioProcessorEditor)
+};
